19c1e8b27ecb1fcb9da28b44b23c82e16f21dd86
Samy Pessé <samypesse@gmail.com>
2014-12-19 00:31:08 +0100

    Adapt and correct tests

--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,10 @@
+var assert = require('assert');
+var hash = require('object-hash');
 var Filterable = require("../lib");
+
+global.assertObjects = function(o1, o2) {
+    assert.equal(hash.sha1(o1), hash.sha1(o2));
+};
 
 global.filter = new Filterable({
     fields: {
--- test/queries.js
+++ test/queries.js
@@ -1,13 +1,11 @@
-var assert = require("assert");
-
 describe('Queries', function() {
-    it('can convert =', function(done) {
-        assert.equal(filter.query("name:Samy").name, "Samy");
+    it('can convert =', function() {
+        assertObjects(filter.query("name:Samy"), { name: { '$eq': 'Samy' } });
     });
-    it('can convert NOT', function(done) {
-        assert.equal(filter.query("NOT name:Samy").name, "Samy");
+    it('can convert NOT', function() {
+        assertObjects(filter.query("NOT name:Samy"), { name: { '$neq': 'Samy' } });
     });
-    it('can convert >=', function(done) {
-        assert.equal(filter.query("followers:>=100").name, "Samy");
+    it('can convert >=', function() {
+        assertObjects(filter.query("followers:>=100"), { followers: { '$gte': '100' } });
     });
 });
