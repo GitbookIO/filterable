0f92da9699cf972be5b7047810f4eca0d164c441
Samy Pessé <samypesse@gmail.com>
2014-12-18 23:47:18 +0100

    Add base to convert query to mongo queries

--- test/queries.js
+++ test/queries.js
@@ -1,7 +1,13 @@
 var assert = require("assert");
 
 describe('Queries', function() {
-    it('can convert equalities', function(done) {
+    it('can convert =', function(done) {
         assert.equal(filter.query("name:Samy").name, "Samy");
+    });
+    it('can convert NOT', function(done) {
+        assert.equal(filter.query("NOT name:Samy").name, "Samy");
+    });
+    it('can convert >=', function(done) {
+        assert.equal(filter.query("followers:>=100").name, "Samy");
     });
 });
